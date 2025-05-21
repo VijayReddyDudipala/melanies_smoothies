4505195ea7457439064cd0ba2787490dfd47e55e
Vijay <vijaydudipala88@gmail.com>
2025-05-20 20:45:27 -0500

    Update streamlit_app.py added container for api response

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,5 +43,7 @@ if ingredients_list:
     #st.write(my_insert_stmt)
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+
 
