06a17f3d74d07cfbb899cfb9ee301b4bdca873ff
Vijay <vijaydudipala88@gmail.com>
2025-05-20 20:48:37 -0500

    Update streamlit_app.py moved to display multiple container for api resp

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 #from snowflake.snowpark.context import get_active_session
 
@@ -26,7 +27,8 @@ if ingredients_list:
     ingredients_string = ''
     for each_fruit in ingredients_list:
         ingredients_string += each_fruit + ' '
-
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
     #st.write(ingredients_string)
     
 
@@ -41,9 +43,7 @@ if ingredients_list:
        st.success("Your smoothie is ordered, "+name_on_order+"!");
 
     #st.write(my_insert_stmt)
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+
+
 
 
